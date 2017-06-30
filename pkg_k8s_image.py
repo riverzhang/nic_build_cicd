6e6b12fa90ad93aa7f8e72529ee44b530777c570
zhengchuan hu <zhengchuan.hu@easystack.cn>
2017-07-01 02:05:40 +0800

    update script

--- pkg_k8s_image.py
+++ pkg_k8s_image.py
@@ -36,6 +36,7 @@ images = [
 
 
 os.system("rm %s -rf&& mkdir %s" % (target_dir, target_dir))
+os.system("rm %s " % (pkg))
 for image_entry in images:
     print(repo_entry)
     image_url = '%s/%s' % (host, image_entry)
